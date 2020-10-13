4fe47ab7e783909677121c37e1c62c8b5c5b77ab
Ethan <ethan.lv@feixitek.com>
2020-10-13 15:38:41 +0800

    use file

--- cli.py
+++ cli.py
@@ -5,7 +5,8 @@ import contextlib
 import time
 
 while True:
-    with contextlib.closing(mmap.mmap(-1, 1024, tagname='test', access=mmap.ACCESS_READ)) as m:
-        s = m.read(1024).replace('\x00', '')
-        print (s)
+    with open('test.dat', 'r') as f:
+        with contextlib.closing(mmap.mmap(f.fileno(), 1024, access=mmap.ACCESS_READ)) as m:
+            s = m.read(1024).replace('\x00', '')
+            print(s)
     time.sleep(1)
--- share_module.py
+++ share_module.py
@@ -4,9 +4,15 @@ import mmap
 import contextlib
 import time
 
-with contextlib.closing(mmap.mmap(-1, 1024, tagname='test', access=mmap.ACCESS_WRITE)) as m:
-    for i in range(1, 10001):
-        m.seek(0)
-        m.write("msg " + str(i))
-        m.flush()
-        time.sleep(1)
+with open("test.dat", "w") as f:
+    f.write('\x00' * 1024)
+
+with open('test.dat', 'r+') as f:
+    with contextlib.closing(mmap.mmap(f.fileno(), 1024, access=mmap.ACCESS_WRITE)) as m:
+        for i in range(1, 10001):
+            m.seek(0)
+            s = "msg " + str(i)
+            s.rjust(1024, '\x00')
+            m.write(s)
+            m.flush()
+            time.sleep(1)
